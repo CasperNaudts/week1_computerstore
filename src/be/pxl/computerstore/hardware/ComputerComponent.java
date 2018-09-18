2ef29af7244c03af4d63de40d3784cab73980569
Unknown <cn230799@stud.miniemeninstituut.be>
2018-09-18 18:27:54 +0200

    ...

--- src/be/pxl/computerstore/hardware/ComputerComponent.java
+++ src/be/pxl/computerstore/hardware/ComputerComponent.java
@@ -1,14 +1,19 @@
 package be.pxl.computerstore.hardware;
 
+import java.util.Random;
+
 public abstract class ComputerComponent {
     private String vendor;
     private String name;
     private double price;
+    private String articleNumber;
+    private Random random = new Random(99999);
 
     public ComputerComponent(String vendor, String name, double price) {
         this.vendor = vendor;
         this.name = name;
         this.price = price;
+        this.articleNumber = vendor.substring(3) + "-" + random.nextInt();
     }
 
     public String getVendor() {
